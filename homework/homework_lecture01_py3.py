51cb14dfb81646023acd42b4747010b775139f70
ethandpeacock <ethan.d.peacock@gmail.com>
2023-07-23 14:05:58 -0500

    Add TODOs for homework1

---
+++ homework/homework_lecture01_py3.py
@@ -0,0 +1,15 @@
+# Homework 01
+
+# TODO: 1. Name one difference between Python2 and Python3. Print your answer as a string in Python
+# TODO: 2. You are given a list x = [0, 1, 2, 3, 4, 5, 6] Print the third item in the list x.
+# TODO: 3. Assign y as the reversed order of list x. Print y.
+# TODO: 4. Use list slicing to assign z [1, 3, 5] from x. Print z
+# TODO: 5. copy incorrect python code that returns invalid syntax error and correct it
+
+'''
+Incorrect syntax error code:
+
+x = 99
+if (x > 0) is True
+print('x is positive')
+'''
