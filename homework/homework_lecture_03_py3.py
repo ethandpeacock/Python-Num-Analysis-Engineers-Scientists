80247a1e0933a5c1334b484dc867215a5e5e7f88
ethandpeacock <ethan.d.peacock@gmail.com>
2023-07-24 16:35:43 -0500

    Update Solutions up to problem 4

--- homework/homework_lecture_03_py3.py
+++ homework/homework_lecture_03_py3.py
@@ -11,6 +11,9 @@ print("\n\n\n")
 import os
 hostname = "ufl.edu"
 response = os.system("ping -c 2 " + hostname)
+print()
+
+
 
 
 # TODO: 2. Use the os library to print the current Python working directory
@@ -23,9 +26,63 @@ response = os.system("ping -c 2 " + hostname)
 # else:
 #    print(f"{hostname} is down!")
 
+
+# 3.
+import math
+import numpy as np
+np_pi = np.pi
+math_pi = math.pi
+print("numpy pi and math pi are equivalent:")
+if np_pi == math_pi:
+    print(True)
+else:
+    print(False)
+
+
+
 # TODO: 3. Compare math.pi to numpy.pi
 # are these two representations of pi equal?
 # print boolean statement true if they are otherwise false
+
+
+
+
+
+
+print()
+print()
+print()
+print()
+print("sphere\n\n\n")
+
+
+
+
+
+
+
+# 4.
+class sphere:
+    def __init__(self, radius=0.0, mass=0.0):
+        self.radius = radius 
+        self.mass = mass
+    def v(self):
+        return (4/3) * np.pi * self.radius**3
+    def A(self):
+        return 4 * np.pi * self.radius**2
+    def p(self):
+        return (self.mass / self.v())
+
+red = sphere(1.7, 0.25)
+print(dir(red))
+print("\n\nSphere volume:")
+print(red.v())
+print("\n\nSphere surface area:")
+print(red.A())
+print("\n\nSphere density:")
+print(red.p())
+
+
 
 # TODO: 4. Create a class called a sphere.
 # the object sphere requires a radius and mass to initialize
